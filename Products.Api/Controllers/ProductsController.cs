a5c5a6b2afcf6d6eac72111749a7195b9fb735fb
petrmatej94 <P.Matej94@gmail.com>
2025-03-25 21:26:44 +0100

    Add Swagger XML documentation

--- Products.Api/Controllers/ProductsController.cs
+++ Products.Api/Controllers/ProductsController.cs
@@ -18,7 +18,14 @@ public class ProductsController : ControllerBase
 		_productService = productService;
 	}
 
+	/// <summary>
+	/// Get product by ID
+	/// </summary>
+	/// <param name="id">Product ID</param>
+	/// <returns>Product if found, 404 otherwise</returns>
 	[HttpGet(ApiEndpoints.Products.Get)]
+	[ProducesResponseType(typeof(ProductResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<IActionResult> Get([FromRoute] Guid id)
 	{
 		Product? product = await _productService.GetByIdAsync(id);
@@ -31,7 +38,12 @@ public class ProductsController : ControllerBase
 		return Ok(response);
 	}
 
+	/// <summary>
+	/// Get all products
+	/// </summary>
+	/// <returns>List of products (can be empty)</returns>
 	[HttpGet(ApiEndpoints.Products.GetAll)]
+	[ProducesResponseType(typeof(IEnumerable<ProductResponse>), StatusCodes.Status200OK)]
 	public async Task<IActionResult> GetAll()
 	{
 		IEnumerable<Product> products = await _productService.GetAllAsync();
@@ -39,7 +51,14 @@ public class ProductsController : ControllerBase
 		return Ok(response);
 	}
 
+	/// <summary>
+	/// Creates a new product
+	/// </summary>
+	/// <param name="request">Product creation data</param>
+	/// <returns>The newly created product.</returns>
 	[HttpPost(ApiEndpoints.Products.Create)]
+	[ProducesResponseType(typeof(ProductResponse), StatusCodes.Status201Created)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	public async Task<IActionResult> Create([FromBody] CreateProductRequest request)
 	{
 		Product? product = request.MapToProduct();
@@ -49,7 +68,16 @@ public class ProductsController : ControllerBase
 		return CreatedAtAction(nameof(Get), new { id = product.Id }, response);
 	}
 
+	/// <summary>
+	/// Updates product stock information
+	/// </summary>
+	/// <param name="id">ID of the product to update</param>
+	/// <param name="request">Stock update payload</param>
+	/// <returns>The updated product or appropriate error</returns>
 	[HttpPatch(ApiEndpoints.Products.Patch)]
+	[ProducesResponseType(typeof(ProductResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<IActionResult> PatchStock([FromRoute] Guid id, [FromBody] PatchProductStockRequest request)
 	{
 		if (request is null)
